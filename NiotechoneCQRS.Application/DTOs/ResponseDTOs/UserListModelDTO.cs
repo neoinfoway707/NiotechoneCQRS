19adb7d10c2e0ac8dc9c7fd67adf4944f9e2ac7b
divyesh.patel <divyesh.patel8@appinventiv.com>
2025-12-30 18:58:36 +0530

    Code changes

---
+++ NiotechoneCQRS.Application/DTOs/ResponseDTOs/UserListModelDTO.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiotechoneCQRS.Application.DTOs.ResponseDTOs
+{
+    public class UserListModelDTO
+    {
+        public IList<UserModel> UserList { get; set; }
+        public UserFiltersModel ParamsCompany { get; set; }
+        public int TotalRecord { get; set; }
+    }
+
+    public class UserFiltersModel
+    {
+        public string SearchExpression { get; set; }
+        public string SortExpression { get; set; }
+        public string SortDirection { get; set; }
+        public int? StartIndex { get; set; }
+        public int? PageSize { get; set; }
+    }
+
+    public class UserColumnFilters
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string UserRole { get; set; }
+        public string Status { get; set; }
+        public string CompanyName { get; set; }
+        public string GeneralSearch { get; set; }
+        public string StatusId { get; set; }
+        public string CompanyId { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public bool IsReport { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UserModel
+    {
+        public long UserId { get; set; }
+        public string UserIdEncrypt { get; set; }
+        public long CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public long? UserRoleId { get; set; }
+        public string Address { get; set; }
+        public string CountryName { get; set; }
+        public int? CountryId { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+        public int StatusId { get; set; }
+        [NotMapped]
+        public string StatusName { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public long? CreatedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string LastLoginDate { get; set; }
+        public long? ModifyBy { get; set; }
+        public string LastLoginCompany { get; set; }
+        public int? UserTypeId { get; set; }
+        public int? PersonalTypeId { get; set; }
+        public byte? UserLoginType { get; set; }
+        public string LoginGUID { get; set; }
+    }
+}
---
+++ NiotechoneCQRS.Web/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using NiotechoneCQRS.Application.DTOs.ResponseDTOs;
+using NiotechoneCQRS.Domain.Entities;
+
+namespace NiotechoneCQRS.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApiClient _apiClient;
+        public UserController(ApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            string url = "https://localhost:7121/get-all-users";
+
+            var response = await _apiClient.GetAsync<ResponseDTO<UserListModelDTO>>(
+               url);
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetUserList(DataSourceRequest request, long? companyId)
+        {
+                        UserColumnFilters columnFilters = new UserColumnFilters();
+            var userList = await _apiClient.GetAsync<UserListModelDTO>(
+               "/api/users");
+            var userView = (userList?.UserList ?? Enumerable.Empty<UserModel>()).Select(x => new UserModel
+            {
+                CompanyId = x.CompanyId,
+                UserId = x.UserId,
+                FullName = x.FullName,
+                UserName = x.UserName,
+                StatusName = x.StatusName,
+                Email = x.Email,
+                Phone = x.Phone,
+                Address = x.Address,
+                LastLoginDate = x.LastLoginDate,
+                PersonalTypeId = x.PersonalTypeId
+            }).AsQueryable();
+
+            DataSourceResult result = await userView.ToDataSourceResultAsync(request);
+
+            return Json(result);
+        }
+
+    }
+}
